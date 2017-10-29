8dc218900826f706ebed6ba3a583d418c632ef98
Andrey Radantsev <avradantsev@yandex.ru>
2017-10-29 14:58:21 +0300

    Added basic logic of app

--- app.js
+++ app.js
@@ -6,9 +6,39 @@ const bot = new telegramBot(token, {
     polling: true
 });
 
+function start(chatId) {
+    bot.sendMessage(chatId, 'Привет! Я криптоБот. \nВыбери валюту, чтобы продолжить!', {
+        reply_markup: JSON.stringify({
+            keyboard: [[{
+                text: 'BTC'
+            }],
+            [{
+                text: 'TOP-5'
+            }]]
+        })
+    });
+}
+
+function sendBTC(chatId) {
+    bot.sendMessage(chatId, 'Курс BTC' + ' составляет 50.000 Р');
+}
+
+function sendTop(chatId) {
+    bot.sendMessage(chatId, 'Курс TOP-5 валют' + ' составляет 50.000 Р');
+}
+
 bot.on('message', function(msg) {
     var chatId = msg.from.id;
-    var file = __dirname + '/photo.jpg';
 
-    bot.sendPhoto(chatId, file);
+    switch (msg.text) {
+        case '/start':
+            start(chatId);
+            break;
+        case 'BTC':
+            sendBTC(chatId);
+            break;
+        case 'TOP-5':
+            sendTop(chatId);
+            break;
+    }
 });
