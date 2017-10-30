5ef204e719daf87fe74082263fea305597c9bfca
Andrey Radantsev <avradantsev@yandex.ru>
2017-10-31 01:48:24 +0300

    Add list for currencies

--- app.js
+++ app.js
@@ -9,39 +9,30 @@ const bot = new telegramBot(token, {
 
 var urlCourse = 'https://api.coinmarketcap.com/v1/ticker/?limit=5';
 
-function sendRequest(chatId, i) {
-    request(urlCourse, function (error, response, body) {
-        // console.log('error:', error); // Print the error if one occurred
-        // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        // console.log('body:', body); // Print the HTML for the Google homepage
-        var data = JSON.parse(body);
-    
-        bot.sendMessage(chatId,
-            data[i].name +' (' + data[i].symbol + ')' + '\nЦена ($): ' + data[i].price_usd + ' $ \nЦена (BTC): ' + data[i].price_btc + ' BTC'
-        );
-    });
-}
-
 function start(chatId) {
     bot.sendMessage(chatId, 'Привет! Я криптоБот. \nВыбери валюту, чтобы продолжить!', {
         reply_markup: JSON.stringify({
             keyboard: [[{
-                text: 'BTC'
-            }],
-            [{
-                text: 'ETH'
-            }],
-            [{
-                text: 'XRP'
-            }],
-            [{
-                text: 'BCH'
-            }],
-            [{
-                text: 'LTC'
+                text: 'Курсы топ-5 криптовалют'
             }]]
         })
     });
+};
+
+function sendRequest(chatId, i) {
+    request(urlCourse, function (error, response, body) {
+       var data = JSON.parse(body);
+    
+        bot.sendMessage(chatId,
+            data[i].name +' (' + data[i].symbol + ')' + '\nЦена ($): ' + data[i].price_usd + '\nЦена (BTC): ' + data[i].price_btc
+        );
+    });
+};
+
+function sendList(chatId) {
+    bot.sendMessage(chatId, 
+        '/BTC\n/ETH\n/XRP\n/BCH\n/LTC'
+    );
 }
 
 bot.on('message', function(msg) {
@@ -51,22 +42,23 @@ bot.on('message', function(msg) {
         case '/start':
             start(chatId);
             break;
-        case 'BTC':
+        case 'Курсы топ-5 криптовалют':
+            sendList(chatId);
+            break;
+        case '/BTC':
             sendRequest(chatId, 0);
             break;
-        case 'ETH':
+        case '/ETH':
             sendRequest(chatId, 1);
             break;
-        case 'XRP':
+        case '/XRP':
             sendRequest(chatId, 2);
             break;
-        case 'BCH':
+        case '/BCH':
             sendRequest(chatId, 3);
             break;
-        case 'LTC':
+        case '/LTC':
             sendRequest(chatId, 4);
             break;
     }
 });
-
-
